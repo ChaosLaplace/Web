b1e25f6a180a0c60496e1360783b1054009cc06a
Chaos <G77654321@gmail.com>
2019-04-09 16:51:09 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const app_express = express();
 var port = process.env.PORT || 8000; //環境變數(執行時給的外部參數PORT=?)或8080(同80,WWW代理服務,實現網頁瀏覽)
 
 var server = app_express.listen(port, () => //listen EADDRINUSE 此錯誤為port已使用
-	{
-		    host = server.address().address;
-		    port = server.address().port;
+{
+    host = server.address().address;
+    port = server.address().port;
 
-		    console.log('-----------------------------------------------------------');
-		    console.log(`Server Access Flash -> http://${host}:${port}`);
-			        console.log('-----------------------------------------------------------');
-	});
+    console.log('-----------------------------------------------------------');
+    console.log(`Server Access Flash -> http://${host}:${port}`);
+    console.log('-----------------------------------------------------------');
+});
 
-		app_express.get('/', function(req, res)
-			{
-			    res.send('Access');
-			});
+app_express.get('/', function(req, res)
+{
+    res.send('Access');
+});
 
