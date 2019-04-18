c9944cf6e85aa57bbc022b864b9efdf2740b2cde
Chaos <G77654321@gmail.com>
2019-04-18 14:28:33 +0800

    Update date.js

--- things/date.js
+++ things/date.js
@@ -5,7 +5,7 @@ module.exports = function()
     var year = date.getFullYear();
     var month = date.getMonth() + 1 >= 10 ? date.getMonth() + 1 : '0' + (date.getMonth() + 1);
     var day = date.getDate() >= 10 ? date.getDate() : '0' + date.getDate();
-    var hour = date.getHours() >= 10 ? date.getHours() : '0' + date.getHours();
+    var hour = date.getHours() + 8 >= 10 ? date.getHours() + 8  : '0' + date.getHours() + 8 ;
     var minute = date.getMinutes() >= 10 ? date.getMinutes() : '0' + date.getMinutes();
     var second = date.getSeconds() >= 10 ? date.getSeconds() : '0' + date.getSeconds();
     var date = year+'/'+month+'/'+day+' '+hour+':'+minute+':'+second;
