ddc1ff3b73c656d178930c430f5f04ddd63ac0fc
Chaos <G77654321@gmail.com>
2019-04-29 21:42:22 +0800

    Update mysql.js

--- app/mysql.js
+++ app/mysql.js
@@ -78,21 +78,3 @@ module.exports =
         });
     }
 };
-
-/*
-SET CHARSET utf8;
-
-use node;
- 
-DROP TABLE IF EXISTS `Session`;
-
-CREATE TABLE `Session`
-(
-  `id` bigint NOT NULL AUTO_INCREMENT COMMENT 'ID',
-  `User` longtext NOT NULL COMMENT '帳號',
-  `Password` longtext NOT NULL COMMENT '密碼',
-  `create_time` datetime NOT NULL COMMENT '作成日時',
-  PRIMARY KEY (`id`)
-)
-ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
-*/
