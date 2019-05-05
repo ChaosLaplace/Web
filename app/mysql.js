7ae5de25ba8e964d082b24f921bc9f014480a52e
Chaos <G77654321@gmail.com>
2019-05-05 21:31:08 +0800

    Update mysql.js

--- app/mysql.js
+++ app/mysql.js
@@ -61,8 +61,6 @@ module.exports =
 
             connect_mysql.query(select_session, function(err, rows, fields)
             {
-                var params = {};
-
                 if(err) 
                 { 
                     console.log('[DB]mysql SELECT -> err');
@@ -81,6 +79,7 @@ module.exports =
                         {
                             console.log('驗證成功');
 
+                            var params = {};
                             params.user = rows[key].User;
                             params.password = rows[key].Password;
 
