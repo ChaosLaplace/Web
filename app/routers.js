f9115af87e86ce006df2d2da0bc32af93b8d6586
Chaos <G77654321@gmail.com>
2019-05-06 14:08:43 +0800

    Add files via upload

--- app/routers.js
+++ app/routers.js
@@ -62,7 +62,7 @@ module.exports = function(app, log)
             {
                 console.log('帳號已存在');
                 
-                res.redirect('http://104.155.206.74/');
+                res.render('mysql', {Date : date(), Session : '帳號已存在,請刷新頁面(F5)'});
             }
             else
             {
