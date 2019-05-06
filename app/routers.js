742787d1e0618960b3ac8f85e90f5273a3644172
Chaos <G77654321@gmail.com>
2019-05-06 14:38:58 +0800

    Add files via upload

--- app/routers.js
+++ app/routers.js
@@ -62,7 +62,7 @@ module.exports = function(app, log)
             {
                 console.log('帳號已存在');
                 
-                res.redirect(200, '/');
+                res.redirect(302, '/');
             }
             else
             {
