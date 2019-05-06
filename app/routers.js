f22f81d8e5bbe421fc73da9345aaca0f43988186
Chaos <G77654321@gmail.com>
2019-05-06 12:26:45 +0800

    Add files via upload

--- app/routers.js
+++ app/routers.js
@@ -43,6 +43,7 @@ module.exports = function(app, log)
             password : req.query.password
         };
 
+        req.session.user = user_session; //cookie紀錄connect.sid
         params = {};
         var mysql_select = select('Session', user_session.user, user_session.password);
         console.log('params -> %s', JSON.stringify(params));
@@ -57,7 +58,6 @@ module.exports = function(app, log)
         else
         {
             console.log('創帳號');
-            req.session.user = user_session; //cookie紀錄connect.sid
             //mysql_db.INSERT('Session', user_session.user, user_session.password);
             res.render('mysql', {Date : date(), Session : '帳號已創建,請刷新頁面(F5)'});
             //req.query -> 獲取URL的參數串
