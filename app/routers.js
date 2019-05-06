bdc1a144a2260655fd9543005fbb28c25a2b6365
Chaos <G77654321@gmail.com>
2019-05-06 10:45:04 +0800

    Add files via upload

--- app/routers.js
+++ app/routers.js
@@ -3,7 +3,7 @@ const mysql = require('mysql');
 
 var date = require('../things/date'); //自製時間格式
 var crypto = require('../things/crypto'); //自製加解密格式
-var mysql = require('./mysql'); //自製db格式
+var mysql_db = require('./mysql'); //自製db格式
 
 module.exports = function(app, log)
 {
@@ -55,7 +55,7 @@ module.exports = function(app, log)
             console.log('創帳號');
             console.log('mysql_select -> %s', JSON.stringify(mysql_select));
             req.session.user = user_session; //cookie紀錄connect.sid
-            //mysql.INSERT('Session', user_session.user, user_session.password);
+            //mysql_db.INSERT('Session', user_session.user, user_session.password);
             res.render('mysql', {Date : date(), Session : '帳號已創建,請刷新頁面(F5)'});
             //req.query -> 獲取URL的參數串
             //res.render('confirm', {user : crypto.decrypt(user_session.user), password : crypto.decrypt(user_session.password)}); //載入confirm.ejs頁面
