6bab2a553b360c6f19a55757af766370c17d2ef1
Chaos <G77654321@gmail.com>
2019-05-06 12:58:11 +0800

    Add files via upload

--- app/routers.js
+++ app/routers.js
@@ -57,7 +57,7 @@ module.exports = function(app, log)
             {
                 console.log('帳號已存在');
                 
-                res.location('/');
+                res.location('login', {Date : date(), Session : 'Seesion -> ' + JSON.stringify(req.session.user)}); //載入index.ejs頁面
             }
             else
             {
